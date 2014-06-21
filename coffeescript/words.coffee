2e7469d981e729cfe6710832ea893767d26bd56a
Unichan <unichan@unichan2.org>
2014-06-21 00:31:44 -0400

    More

--- coffeescript/words.coffee
+++ coffeescript/words.coffee
@@ -10,16 +10,23 @@ sup jelly
 gtfo
 please die
 die in a fire
+shut the fuck up
+nice try
 go back to cc
 go back to tc
 go back to ilb
+go lick loli's tampons
 go eat some sleeping pills
+i can't believe you're this retarded
 go fuck yourself
 kill yourself
+eat a dick
 hurr durr
 nice b8
 nice reverse psychopathology
 how fucking dare you
+1/10 cloxxies
+i hope kenny sits on you
 """
 
 @words.you =
@@ -58,6 +65,7 @@ deaf
 disgusting
 gargantuan
 massive
+yeasty
 ultra
 mega
 ultra mega
@@ -126,12 +134,15 @@ shrimp
 forumfag
 autist
 white knight
+dicknose
 brony
 horsefucker
+goblin
 mlpfag
 weeaboo
 gnome
 daughter bather
+alpha beta
 shitlord
 toad
 goy
--- js/words.js
+++ js/words.js
@@ -2,14 +2,14 @@
 (function() {
   this.words = {};
 
-  this.words.intro = "sup caleb\nsup cecil\nsup loli\nsup jwagzz\nsup jelly\ngtfo\nplease die\ndie in a fire\ngo back to cc\ngo back to tc\ngo back to ilb\ngo eat some sleeping pills\ngo fuck yourself\nkill yourself\nhurr durr\nnice b8\nnice reverse psychopathology\nhow fucking dare you";
+  this.words.intro = "sup caleb\nsup cecil\nsup loli\nsup jwagzz\nsup jelly\ngtfo\nplease die\ndie in a fire\nshut the fuck up\nnice try\ngo back to cc\ngo back to tc\ngo back to ilb\ngo lick loli's tampons\ngo eat some sleeping pills\ni can't believe you're this retarded\ngo fuck yourself\nkill yourself\neat a dick\nhurr durr\nnice b8\nnice reverse psychopathology\nhow fucking dare you\n1/10 cloxxies\ni hope kenny sits on you";
 
   this.words.you = "you\nyew";
 
-  this.words.adjective = "sad\nbeta\nomega\npathetic\nchildish\nprojecting\nautistic\nretarded\ndeluded\nforever alone\npsychotic\nobsessive\ntryhard\ndisgusting\ncreepy\n\"beautiful\"\nbutthurt\nanorexic\ncringeworthy\nvapid\ncatty\njealous\ninsufferable\nobnoxious\ndeaf\ndisgusting\ngargantuan\nmassive\nultra\nmega\nultra mega\nmega ultra";
+  this.words.adjective = "sad\nbeta\nomega\npathetic\nchildish\nprojecting\nautistic\nretarded\ndeluded\nforever alone\npsychotic\nobsessive\ntryhard\ndisgusting\ncreepy\n\"beautiful\"\nbutthurt\nanorexic\ncringeworthy\nvapid\ncatty\njealous\ninsufferable\nobnoxious\ndeaf\ndisgusting\ngargantuan\nmassive\nyeasty\nultra\nmega\nultra mega\nmega ultra";
 
   this.words.compound_adjective = "fat-ass\ncandy-ass\nfaggot-ass\ncactus-fucking\ntwo-faced\ndick-sucking\ndragon dildo-owning\nextra-water-retaining\npointy-kneed\nsharp-elbowed\nfake-titted\nfedora-wearing\ncum-guzzling\nass-kissing\nbutt-fucking\npotato-nosed";
 
-  this.words.noun = "virgin\nneckbeard\npedo\njunefag\njunebot\nsperglord\nfucking retard\naspie fuck\nburlap sack\npeterfag\ncuckold\ncatiebot\nstalker\ncunt\npussy\njew\nfaggot\nfag\nnamefag\ntripfag\nfeminazi\nattention whore\nhappy merchant\nkike\nhooknose\nshill\ncouncilfag\nfeminist\nholocaust believer\nfucking psycho\ntroll\ncaleb\ncecil 2.0\nretard\nfaggot son\nsamefag\nshrimp\nforumfag\nautist\nwhite knight\nbrony\nhorsefucker\nmlpfag\nweeaboo\ngnome\ndaughter bather\nshitlord\ntoad\ngoy\nmale OC";
+  this.words.noun = "virgin\nneckbeard\npedo\njunefag\njunebot\nsperglord\nfucking retard\naspie fuck\nburlap sack\npeterfag\ncuckold\ncatiebot\nstalker\ncunt\npussy\njew\nfaggot\nfag\nnamefag\ntripfag\nfeminazi\nattention whore\nhappy merchant\nkike\nhooknose\nshill\ncouncilfag\nfeminist\nholocaust believer\nfucking psycho\ntroll\ncaleb\ncecil 2.0\nretard\nfaggot son\nsamefag\nshrimp\nforumfag\nautist\nwhite knight\ndicknose\nbrony\nhorsefucker\ngoblin\nmlpfag\nweeaboo\ngnome\ndaughter bather\nalpha beta\nshitlord\ntoad\ngoy\nmale OC";
 
 }).call(this);
